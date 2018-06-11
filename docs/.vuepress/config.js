7eb1be4d7fc0a8164e5d97c28eac0e3a1bebb07f
shoestrong <zqc_2007@yeah.net>
2018-06-11 14:22:27 +0800

    配置

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,30 @@
+module.exports = {
+    base: '/',                                      // 部署站点的基础路径
+    title: 'snippets',                              // 网站的标题
+    description: '代码片段收集',                      // 网站的描述
+    head: [                                         // head标签
+        ['link', { rel: 'icon', href: '/favicon.ico' }]
+    ],
+    host: '0.0.0.0',                                // 主机
+    port: 8020,                                     // 端口
+    dest: '.vuepress/dist',                         // vuepress build 的输出目录
+    ga: '',                                         // 谷歌统计
+    serviceWorker: false,                           // 用于PWA，需https下才能开通
+    locales: undefined,                             // 多语言支持
+    theme: undefined,                               // 主题
+    themeConfig: {                                  // 主题配置
+        nav: [
+            { text: '主页', link: '/' },
+            { text: 'Javascript', link: '/javascript/' },
+            { text: 'CSS', link: '/css/' },
+        ],
+        sidebar: {
+            '/javascript/': [
+                'camelize'
+            ],
+            '/css/': [
+                'font-family'
+            ]
+        }
+    }                                
+}
